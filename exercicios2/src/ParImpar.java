e534d7b8c0d5a018fed3610debe9667d5ec790c1
EduuhSouza <146948069+EduuhSouza@users.noreply.github.com>
2025-08-07 17:08:04 -0300

    Add files via upload

---
+++ exercicios2/src/ParImpar.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ParImpar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escreva um número");
+        int numero1 = scanner.nextInt();
+
+        System.out.println("Escreva o segundo número maior que o primeiro");
+        int numero2 = scanner.nextInt();
+
+        System.out.println("Escolha par ou impar");
+        
+
+
+    }
+}
